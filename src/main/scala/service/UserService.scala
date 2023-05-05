9725cdb3c7ac6c309230f49036e10f74ea5aedde
eren <eozel44@gmail.com>
2023-05-05 18:35:26 +0300

    service layer

---
+++ src/main/scala/service/UserService.scala
@@ -0,0 +1,56 @@
+package eozel.service
+
+import doobie.Transactor
+import doobie.implicits._
+import eozel.config.AppConfig
+import eozel.domain.error.{AppDaoError, AppError}
+import eozel.domain.user._
+import eozel.persistence.user._
+import zio._
+import zio.interop.catz._
+import zio.macros.accessible
+
+@accessible
+trait UserService {
+
+  def signupUser(user: User, role: Role): ZIO[Any, AppError, User]
+
+}
+
+case class UserServiceLive(
+  appConfig: AppConfig,
+  userRepo: UserRepository,
+  userRoleRepo: UserRoleRepository,
+  tx: Transactor[Task]
+) extends UserService {
+
+  override def signupUser(user: User, role: Role): IO[AppError, User] = {
+
+    val tranzactIO = for {
+      id   <- userRepo.createIO(user)
+      _    <- userRoleRepo.createUserRoleIO(new UserRole(id, role))
+      user <- userRepo.getIO(id)
+    } yield user
+
+    tranzactIO.transact(tx).orDie.flatMap {
+      case Some(item) => ZIO.succeed(item)
+      case None       => ZIO.fail(AppDaoError("signup error"))
+    }
+
+  }
+
+}
+
+object UserServiceLive {
+
+  val layer: ZLayer[Has[AppConfig] with Has[UserRepository] with Has[UserRoleRepository] with Has[
+    Transactor[Task]
+  ], Nothing, Has[UserService]] = ZLayer.fromServices[
+    AppConfig,
+    UserRepository,
+    UserRoleRepository,
+    Transactor[Task],
+    UserService
+  ](new UserServiceLive(_, _, _, _))
+
+}
