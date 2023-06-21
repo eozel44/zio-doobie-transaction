2cfa37911d61acd519675e0c9380e8a19984ded9
eren <eozel44@gmail.com>
2023-06-21 16:17:13 +0300

    remove codec

--- src/main/scala/eozel/domain/Error.scala
+++ src/main/scala/eozel/domain/Error.scala
@@ -1,25 +1,8 @@
 package eozel.domain
 
-import io.circe._
-import io.circe.generic.JsonCodec
-import io.circe.syntax._
+sealed trait AppError                          extends Throwable
+case class AppDaoError(message: String)        extends RuntimeException(message) with AppError
+case class AppConfigError(message: String)     extends IllegalStateException(message) with AppError
+case class AlreadyExistsError(message: String) extends RuntimeException(message) with AppError
+case class NotFoundError(message: String)      extends RuntimeException(message) with AppError
 
-@JsonCodec sealed trait AppError                          extends Throwable
-@JsonCodec case class AppDaoError(message: String)        extends RuntimeException(message) with AppError
-@JsonCodec case class AppConfigError(message: String)     extends IllegalStateException(message) with AppError
-@JsonCodec case class AlreadyExistsError(message: String) extends RuntimeException(message) with AppError
-@JsonCodec case class NotFoundError(message: String)      extends RuntimeException(message) with AppError
-
-object AppError {
-  implicit val decodeAppError: Decoder[AppError] = Decoder[AppDaoError]
-    .map[AppError](identity)
-    .or(Decoder[AppConfigError].map[AppError](identity))
-    .or(Decoder[AlreadyExistsError].map[AppError](identity))
-
-  implicit val encodeAppError: Encoder[AppError] = Encoder.instance {
-    case e1 @ AppDaoError(_)        => e1.asJson
-    case e2 @ AppConfigError(_)     => e2.asJson
-    case e3 @ AlreadyExistsError(_) => e3.asJson
-    case e4 @ NotFoundError(_)      => e4.asJson
-  }
-}
